1d497446cced4505da188e67722537130cc46440
prekshagoyani <117463800+prekshagoyani@users.noreply.github.com>
2022-11-04 08:10:05 -0700

    Add files via upload

---
+++ pr_1.py
@@ -0,0 +1,19 @@
+from html.entities import name2codepoint
+
+
+print("hello preksha")
+print("my name preksha") 
+
+names={"name1":"preksha","name2":"devanshi"}
+print(names["name1"])
+ 
+def multiply(i):
+    return i * i
+
+x = map(multiply,(3,4,5,6))
+print(list(x))
+
+
+example = ["Welco","to","Sem 3"]
+x = list(map(len, example))
+print(x)
